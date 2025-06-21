6161b631a126571fda8d61956d9ef492bb8e9a76
kim-se-hee <saree98@nvaer.com>
2025-06-21 23:16:32 +0900

    init: 최초 커밋

---
+++ src/main/java/ksh/deliverymate/DeliveryMateApplication.java
@@ -0,0 +1,13 @@
+package ksh.deliverymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ksh/deliverymate/DeliveryMateApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.deliverymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
