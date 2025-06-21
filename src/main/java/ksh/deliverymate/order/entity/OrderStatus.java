c09d290e2c5c5d7494e07ca21f01d53ac049d072
kim-se-hee <saree98@nvaer.com>
2025-06-22 00:55:06 +0900

    feat: 주문 관련 엔티티 생성

---
+++ src/main/java/ksh/deliverymate/order/entity/OrderStatus.java
@@ -0,0 +1,12 @@
+package ksh.deliverymate.order.entity;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    ACCEPTED,
+    ASSIGNED,
+    DELIVERING,
+    COMPLETED,
+    CANCELLED,
+    REJECTED
+}
