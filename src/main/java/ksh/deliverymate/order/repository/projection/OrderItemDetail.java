c22941ce57252bd595cba893aa506dcafc324536
kim-se-hee <saree98@nvaer.com>
2025-06-22 19:45:16 +0900

    feat: 주문 정보 상세 조회 쿼리를 위한 프로젝션용 dto 생성

---
+++ src/main/java/ksh/deliverymate/order/repository/projection/OrderItemDetail.java
@@ -0,0 +1,12 @@
+package ksh.deliverymate.order.repository.projection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderItemDetail {
+
+    private String menuName;
+    private int quantity;
+}
