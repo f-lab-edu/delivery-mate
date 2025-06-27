479525b603b1a114781aa663ec9c36d577608335
kim-se-hee <saree98@nvaer.com>
2025-06-27 22:05:12 +0900

    feat: id로 라이더 미배정 주문 조회 기능 구현

--- src/main/java/ksh/deliverymate/order/repository/OrderRepository.java
+++ src/main/java/ksh/deliverymate/order/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package ksh.deliverymate.order.repository;
 
+import jakarta.persistence.LockModeType;
 import ksh.deliverymate.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderQueryRepository {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Order> findByIdAndRiderIdIsNull(long id);
 }
