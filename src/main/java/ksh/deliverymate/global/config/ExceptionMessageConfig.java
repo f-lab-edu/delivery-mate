cada65db5c107b2a6ed1d8fce6af1436e9a08588
kim-se-hee <saree98@nvaer.com>
2025-06-29 01:17:32 +0900

    feat: MessageSource 설정 추가

--- src/main/java/ksh/deliverymate/global/config/ExceptionMessageConfig.java
+++ src/main/java/ksh/deliverymate/global/config/ExceptionMessageConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
 @Configuration
 public class ExceptionMessageConfig {
@@ -16,6 +17,8 @@ public class ExceptionMessageConfig {
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultLocale(Locale.KOREA);
         return messageSource;
     }
 }
